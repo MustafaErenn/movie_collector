10bf8de9d43b1fa90d25a14c99da897e3c928183
Mustafa EREN <54962045+MustafaErenn@users.noreply.github.com>
2021-09-13 15:01:54 +0300

    Update toprated_service.dart

--- lib/product/service/toprated_service.dart
+++ lib/product/service/toprated_service.dart
@@ -13,10 +13,10 @@ class TopRatedService {
     String url;
     if (turkLang) {
       url =
-          'https://api.themoviedb.org/3/movie/top_rated?api_key=3af7f4422f5644de486084c74816093a&page=$page&language=tr';
+          'https://api.themoviedb.org/3/movie/top_rated?api_key=XXX&page=$page&language=tr';
     } else {
       url =
-          'https://api.themoviedb.org/3/movie/top_rated?api_key=3af7f4422f5644de486084c74816093a&page=$page';
+          'https://api.themoviedb.org/3/movie/top_rated?api_key=XXX&page=$page';
     }
 
     final response = await dio.get(url);
